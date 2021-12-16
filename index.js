37466fc39d393a11e6af0316cbcc794391a5e2df
unknown <LPT44@BellMedEx.com>
2021-12-16 19:30:49 +0500

    address module test

--- index.js
+++ index.js
@@ -2,18 +2,51 @@ const express = require("express");
 const os = require("os");
 const cors = require("cors");
 const app = express();
+const address = require("address");
 app.use(cors());
 let PORT = process.env.PORT || 2000;
 
 app.get("/getInfo", (req, res) => {
-  let obj = os.networkInterfaces();
-  let finalObj = {
-    Ethernet: obj.Ethernet,
-    IPv4: obj["Loopback Pseudo-Interface 1"],
-    IPv6: obj["vEthernet (Default Switch)"],
-    hostname: os.hostname,
-  };
-  res.send(finalObj);
+  console.log();
+  console.log();
+  // '192.168.0.2'
+  // 'fe80::7aca:39ff:feb0:e67d'
+  let mac = null;
+  let mac1 = null;
+  address.mac(function (err, addr) {
+    mac = addr; // '78:ca:39:b0:e6:7d'
+  });
+
+  // local loopback
+  console.log(address.ip("lo")); // '127.0.0.1'
+
+  // vboxnet MAC
+  address.mac("vboxnet", function (err, addr) {
+    mac1 = addr; // '0a:00:27:00:00:00'
+  });
+  res.send({
+    addressnpm: {
+      ip: address.ip(),
+      IPv6: address.ipv6(),
+      addressIP: address.ip("lo"),
+      mac,
+      mac1,
+    },
+    ips: req.ips,
+    localaddress: req.socket.localAddress,
+    localPort: req.socket.localPort,
+    family: req.socket.remoteFamily,
+  });
+  // res.send("done");
+
+  // let obj = os.networkInterfaces();
+  // let finalObj = {
+  //   Ethernet: obj.Ethernet,
+  //   IPv4: obj["Loopback Pseudo-Interface 1"],
+  //   IPv6: obj["vEthernet (Default Switch)"],
+  //   hostname: os.hostname,
+  // };
+  // res.send(finalObj);
 });
 app.listen(PORT, () => {
   console.log("running at port", PORT);
